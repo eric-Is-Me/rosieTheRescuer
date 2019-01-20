eb9a97c11bf5443885c600cb8f3b9e1b499512cd
Eric Chaing <chaingisme@gmail.com>
2019-01-20 10:12:04 -0800

    Map is still not loading. Added assets.

--- game.js
+++ game.js
@@ -1,32 +1,35 @@
-var config = {
-    type: Phaser.AUTO,
-    width: 4000,
-    height: 600,
-    backgroundColor: '#2d2d2d',
-    parent
-}
 class mainScene {
 	preload() {
-		this.load.image('player', 'assets/player.png');
-		this.player = this.physics.add.sprite(100,100,'player');
+        this.load.tilemapTiledJSON('tilemap', 'assets/level1.json');
+		this.load.image('rosie', 'assets/rosie.png');
 		this.load.image('coin', 'assets/coin.png');
-		this.coin = this.physics.add.sprite(300,300,'coin');
-        this.load.tilemapTiledJSON('map1', 'map.json');
-        this.load.image('tiles1', 'assets/map1.png');
+        this.load.image('tiles1', 'assets/PNG/jungle_pack_05.png');
 	}
 
 	create() {
 		this.score = 0;
 		let style = { font: '20px Arial', fill: '#fff' };
 		this.scoreText = this.add.text(20, 20, 'score: ' + this.score, style);
+        this.player = this.physics.add.sprite(100,100,'rosie');
+		this.coin = this.physics.add.sprite(300,300,'coin');
         this.player.body.gravity.y = 500;
         this.player.body.checkCollision.up = true;
         this.player.body.checkCollision.right = true;
         this.player.body.checkCollision.left = true;
         
-        var map1 = this.make.tilemap({key: 'map1'});
-        var tileset1 = map1.addTilesetImage('level1', 'tiles1');
-        var layer1 = map1.createStaticLayer('World1', tileset1, 0, 0);
+        var map = this.add.tilemap('tilemap');
+        map.addTilesetImage('GroundLayer', 'tiles1');
+        
+        /*this.map = this.add.tilemap('tilemap');
+        this.map.addTilesetImage('centerGround', 'tiles1');*/
+        
+        var groundLayer = map.createStaticLayer('GroundLayer');
+        console.log(groundLayer);
+        //map.setCollisionBetween(1, 100, true, 'GroundLayer');
+        
+        groundLayer.resizeWorld();
+        
+        this.game.camera.follow(this.player);
 	}
 
 	hit() {
@@ -47,6 +50,8 @@ class mainScene {
 	}
 
 	update() {
+        //this.game.physics.arcade.collide(this.player, this.groundLayer);
+        
 		if(this.physics.overlap(this.player, this.coin)) {
 			this.hit();
 		}
@@ -62,8 +67,9 @@ class mainScene {
         }
 	}
 }
-var game = new Phaser.Game({
-    width: 4000,
+
+new Phaser.Game({
+    width: 800,
     height: 700,
     backgroundColor: '#3498db',
     scene: mainScene,
