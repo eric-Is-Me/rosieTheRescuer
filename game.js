070e258ba7f08799ae00eeac0616308a1d091952
Nathan Brennan <nathandbrennan@gmail.com>
2019-01-20 11:50:58 -0800

    Boundary adjustments

--- game.js
+++ game.js
@@ -62,9 +62,9 @@ class mainScene {
 		}
         this.arrow = this.input.keyboard.createCursorKeys();
         
-        if(this.player.y >= 670) {
+        if(this.player.y >= 470) {
             this.player.body.gravity.y = 0;
-            this.player.y = 670;
+            this.player.y = 470;
         }
         else
             this.player.body.gravity.y = 500;
